b75df881317546404c962e98209c8cacfba5ff81
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-04 10:49:25 -0600

    Added a smaple seed for a few cities so we can test its functionality. Added a connection to the model as well to link the files appropriately.

--- Main/models/index.js
+++ Main/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Project = require('./Project');
+const City = require('./City')
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
@@ -10,4 +11,4 @@ Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Project, City };
