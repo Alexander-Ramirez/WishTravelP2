dd101489787841e3e2cc0b56843657c810da20bd
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-04 12:44:46 -0600

    Reorganized files to match the group's

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -2,7 +2,6 @@ const sequelize = require('../project2/config/connection');
 const { User, City } = require('../models');
 
 const userData = require('./userData.json');
-// const projectData = require('./projectData.json');
 const cityData = require('./cityData.json');
 
 const seedDatabase = async () => {
@@ -12,13 +11,6 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
-  // for (const project of projectData) {
-  //   await Project.create({
-  //     ...project,
-  //     user_id: users[Math.floor(Math.random() * users.length)].id,
-  //   });
-  // };
 
   for (const city of cityData) {
     await City.create({
