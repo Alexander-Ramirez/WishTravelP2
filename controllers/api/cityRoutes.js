d3f2e96eb4dab71d5958d93c5d39d5d253ce7cb7
Matt Reynolds <mattreynolds53@gmail.com>
2022-01-05 12:12:25 -0600

    Alex took control of Matt's screen and was leading this seciton of the pair programing. We added a search bar and fixed routes.

--- controllers/api/cityRoutes.js
+++ controllers/api/cityRoutes.js
@@ -12,6 +12,7 @@ router.post('/', withAuth, async (req, res) => {
     res.status(200).json(newCity);
   } catch (err) {
     res.status(400).json(err);
+    console.log(err)
   }
 });
 
@@ -32,6 +33,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     res.status(200).json(cityData);
   } catch (err) {
     res.status(500).json(err);
+    console.log(err)
   }
 });
 
